8239d7bad2e7de4937bf7a41e1e9361803102f32
Alexandre FOURNIER <alexorf2000@gmail.com>
2022-07-30 21:07:38 +0200

    Update

--- SpeechToText/Form1.cs
+++ SpeechToText/Form1.cs
@@ -17,48 +17,6 @@ namespace SpeechToText
         public Form1()
         {
             InitializeComponent();
-            SetStyle(ControlStyles.SupportsTransparentBackColor, true);
-
-            BackColor = Color.FromArgb(1, 1, 1);
-            TransparencyKey = Color.FromArgb(1, 1, 1);
-
-            FormBorderStyle = FormBorderStyle.None;
-            TopMost = true;
-
-            Size = new Size(1000, 200);
-            
-            CenterToScreen();
-            Location = new Point(Location.X, Location.Y + Screen.PrimaryScreen.WorkingArea.Height / 2 - 200);
-
-            textLabel.BackColor = Color.Transparent;
-
-            UpdateLabelLocation();
         }
-
-        private void UpdateLabelLocation()
-        {
-            textLabel.Location = new Point(Size.Width/2 - textLabel.Size.Width/2, Size.Height / 2 - textLabel.Size.Height / 2);
-        }
-
-        public void UpdateLabelText(string textToPut)
-        {
-            UpdateLabelLocation();
-            if (erase)
-            {
-                erase = false;
-                textLabel.Text = textToPut;
-            }
-            else
-            {
-                Text += textToPut;
-            }
-
-            if (textToPut.Contains('.'))
-            {
-                erase = true;
-            }
-        }
-
-        //protected override void OnPaintBackground(PaintEventArgs e) { }
     }
 }
